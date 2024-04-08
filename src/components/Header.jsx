5a128faac5f233ebdecb9b5ddbfe0279c83204af
doyi <doyi9917@naver.com>
2024-04-08 22:08:23 +0900

    header event add

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,35 @@
-import React from "react";
+import React, { useState, useEffect }from "react";
 import "../styles/common/all.css";
 
 export default function Header() {
+  // 스크롤 위치를 저장하기 위한 상태
+  const [lastScrollPosition, setLastScrollPosition] = useState(0);
+  const [isClosed, setIsClosed] = useState(false);
+
+  // 스크롤 이벤트 핸들러
+  const handleScroll = () => {
+    const currentScrollPosition = window.scrollY;
+
+    if (currentScrollPosition < lastScrollPosition) {
+      setIsClosed(false);
+    } else {
+      setIsClosed(true);
+    }
+    // 마지막 스크롤 위치를 업데이트합니다.
+    setLastScrollPosition(currentScrollPosition);
+  };
+
+  useEffect(() => {
+
+      window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [lastScrollPosition]); // lastScrollPosition이 변경될 때마다 이펙트를 다시 실행합니다.
+
   return (
     <div>
-      <div className="header_wrap">
+      <div className={`header_wrap ${isClosed ? "closed" : ""}`}>
         <div className="header_inner">
           <div className="header_title_wrap">
             <p className="header_title">starting plant</p>
