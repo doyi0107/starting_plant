618f081bd256666dbe80e5dda3596d643bf60524
doyi <doyi9917@naver.com>
2024-04-23 17:15:33 +0900

    index.js revise

--- src/index.js
+++ src/index.js
@@ -4,7 +4,11 @@ import App from './App';
 
 
 async function enableMocking() {
-  if (process.env.NODE_ENV !== "development") {
+  // 로컬 개발 환경에서는 기본적으로 활성화하고, 배포 환경에서는 `REACT_APP_ENABLE_MOCK` 환경 변수를 체크
+  const isDevelopment = process.env.NODE_ENV === "development";
+  const shouldEnableMock = process.env.REACT_APP_ENABLE_MOCK === "true";
+
+  if (!isDevelopment && !shouldEnableMock) {
     return;
   }
 
