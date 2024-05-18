ee86d78e59d3bb42a081bf2185f83d7a51838fc1
kalmaho <66253964+kalmaho@users.noreply.github.com>
2024-05-18 21:14:41 +0900

    get

--- controllers/plants.controller.js
+++ controllers/plants.controller.js
@@ -2,14 +2,13 @@ const Plant = require("../models/plants.model.js");
 
 
 exports.getPlant = (req, res) => {
-    Plant.info(req.params.id, (err, data) => {
+    Plant.info(req.params.plants_id, (err, data) => {
         if (err)
             res.status(500).send({
                 message:
                     err.message || "Some error occurred while retrieving users."
             });
         else {
-            console.log("1")
             res.send(data);
         }
     });
